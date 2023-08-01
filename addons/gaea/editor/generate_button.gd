8a038cd786b8468ed5bfafafcf9d81b47f471101
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2023-07-31 22:37:45 -0300

    Add "clear" button to generators

--- addons/gaea/editor/generate_button.gd
+++ addons/gaea/editor/generate_button.gd
@@ -5,20 +5,42 @@ var button := Button.new()
 
 
 func _init() -> void:
-	anchors_preset = PRESET_HCENTER_WIDE
 	size_flags_horizontal = Control.SIZE_SHRINK_CENTER
 
-	button.text = "Regenerate"
-	button.icon = preload("res://addons/gaea/editor/reload.svg")
-	button.custom_minimum_size.x = 128
-	button.pressed.connect(_on_pressed)
+	var container := HBoxContainer.new()
 
-	add_child(button)
+	container.custom_minimum_size.x = 130
+	add_child(container)
+
+	var generateButton := _add_button(
+		container, "Generate", preload("./reload.svg"), _on_generate_pressed)
+	generateButton.custom_minimum_size.x = 110
+
+	_add_button(container, "Clear", preload("./clear.svg"), _on_clear_pressed)
+
+
+func _add_button(container: Container, text: String, icon: Texture2D, onPressed: Callable) -> Button:
+	var button := Button.new()
+
+	button.text = text
+	button.icon = icon
+
+	button.pressed.connect(onPressed)
+
+	container.add_child(button)
 	add_focusable(button)
 
+	return button
 
-func _on_pressed() -> void:
+
+func _on_generate_pressed() -> void:
 	var object = get_edited_object()
 	if object.has_method("generate"):
 		object.call("generate")
+
+
+func _on_clear_pressed() -> void:
+	var object = get_edited_object()
+	if object.has_method("erase"):
+		object.call("erase")
 
