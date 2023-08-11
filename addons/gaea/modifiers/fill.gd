2fcdd59f8cfaf6d583bdb9b7f2f0e693708b68e5
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2023-08-11 20:05:07 -0300

    Update FIll modifier
    Allows expanding individual sides of the rectangle.

--- addons/gaea/modifiers/fill.gd
+++ addons/gaea/modifiers/fill.gd
@@ -8,17 +8,21 @@ extends Modifier
 ## A [TileInfo] containing information about the filling tile.
 @export var tile: TileInfo
 ## Expands the rect's borders by this amount on both axis.
-@export var expand_rect := 1
+@export_group("Expand", "expand_")
+@export var expand_left := 1
+@export var expand_top := 1
+@export var expand_right := 1
+@export var expand_bottom := 1
 
 
 func apply(grid: Dictionary, _generator: GaeaGenerator) -> Dictionary:
-	var rect
+	var rect: Rect2
 	for tile in grid:
 		if not rect:
 			rect = Rect2(tile, Vector2.ONE)
 		rect = rect.expand(tile)
 
-	rect = rect.grow(expand_rect)
+	rect = rect.grow_individual(expand_left, expand_top, expand_right, expand_bottom)
 
 	for x in range(rect.position.x, rect.end.x + 1):
 		for y in range(rect.position.y, rect.end.y + 1):
