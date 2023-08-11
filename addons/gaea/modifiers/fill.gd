bf30abcc0743087997c75781bf808dcd1ea0de36
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2023-08-11 20:07:37 -0300

    Update variable descriptions

--- addons/gaea/modifiers/fill.gd
+++ addons/gaea/modifiers/fill.gd
@@ -7,11 +7,14 @@ extends Modifier
 
 ## A [TileInfo] containing information about the filling tile.
 @export var tile: TileInfo
-## Expands the rect's borders by this amount on both axis.
 @export_group("Expand", "expand_")
+## Expand the left side of the filled rectangle by this amount.
 @export var expand_left := 1
+## Expand the top side of the filled rectangle by this amount.
 @export var expand_top := 1
+## Expand the right side of the filled rectangle by this amount.
 @export var expand_right := 1
+## Expand the bottom side of the filled rectangle by this amount.
 @export var expand_bottom := 1
 
 
@@ -22,7 +25,9 @@ func apply(grid: Dictionary, _generator: GaeaGenerator) -> Dictionary:
 			rect = Rect2(tile, Vector2.ONE)
 		rect = rect.expand(tile)
 
-	rect = rect.grow_individual(expand_left, expand_top, expand_right, expand_bottom)
+	rect = rect.grow_individual(
+			expand_left, expand_top, expand_right, expand_bottom
+			)
 
 	for x in range(rect.position.x, rect.end.x + 1):
 		for y in range(rect.position.y, rect.end.y + 1):
