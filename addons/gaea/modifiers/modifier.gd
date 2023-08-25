f13b9338fb0386a1935d56e1dee7a0b843b9bd00
titus <80385479+titusio@users.noreply.github.com>
2023-08-25 14:06:36 +0200

    Remove `apply_chunk` method from base modifier class

--- addons/gaea/modifiers/modifier.gd
+++ addons/gaea/modifiers/modifier.gd
@@ -4,7 +4,3 @@ extends Resource
 
 func apply(grid: Dictionary, generator: GaeaGenerator) -> Dictionary:
 	return grid
-
-
-func apply_chunk(grid: Dictionary, generator: GaeaGenerator, chunk_position: Vector2i) -> Dictionary:
-	return grid
