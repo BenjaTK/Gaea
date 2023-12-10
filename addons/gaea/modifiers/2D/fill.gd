abbf1ee93e979d54824db5330a41fed3d6660ecb
benjatk <benjatorreskeselman@gmail.com>
2023-12-09 21:28:18 -0300

    Allow setting affected layers in Fill modifier

--- addons/gaea/modifiers/2D/fill.gd
+++ addons/gaea/modifiers/2D/fill.gd
@@ -8,6 +8,7 @@ extends Modifier2D
 
 ## A [TileInfo] containing information about the filling tile.
 @export var tile: TileInfo
+@export var affected_layers: Array[int] = [0]
 @export_group("Expand", "expand_")
 ## Expand the left side of the filled rectangle by this amount.
 @export var expand_left := 1
@@ -21,7 +22,7 @@ extends Modifier2D
 
 func apply(grid: GaeaGrid, _generator: GaeaGenerator) -> void:
 	var rect: Rect2i
-	for layer in range(grid.get_layer_count()):
+	for layer in affected_layers:
 		for cell in grid.get_cells(layer):
 			if not rect:
 				rect = Rect2i(cell, Vector2i.ONE)
