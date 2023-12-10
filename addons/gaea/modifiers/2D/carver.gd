178c21c36b2b1ed3c1a1fd4028de289a2eddfceb
benjatk <benjatorreskeselman@gmail.com>
2023-12-09 21:25:41 -0300

    Add Carver support

--- addons/gaea/modifiers/2D/carver.gd
+++ addons/gaea/modifiers/2D/carver.gd
@@ -12,27 +12,30 @@ extends ChunkAwareModifier2D
 ## will be deleted from the map. (-1.0 is black, 1.0 is white)[br]
 ## Lower values mean more empty areas.
 @export_range(-1.0, 1.0) var threshold := 0.15
-@export_group("Bounds")
-## Leave any or both axis as [code]inf[/code] to not have any limits.
-@export var max := Vector2(INF, INF)
-## Leave any or both axis as [code]-inf[/code] to not have any limits.
-@export var min := Vector2(-INF, -INF)
+@export_group("Bounds", "bounds_")
+@export var bounds_enabled: bool = false
+@export var bounds_max := Vector2(0, 0)
+@export var bounds_min := Vector2(-0, -0)
+@export var affected_layers: Array[int] = [0]
 
 
 func _apply_area(area: Rect2i, grid: GaeaGrid, _generator: GaeaGenerator) -> void:
 	for x in range(area.position.x, area.end.x + 1):
 		for y in range(area.position.y, area.end.y + 1):
-			var cell := Vector2i(x, y)
-			if not grid.has_cell(cell) or _is_out_of_bounds(cell):
-				continue
+			for layer in affected_layers:
+				var cell := Vector2i(x, y)
+				if not grid.has_cell(cell, layer) or _is_out_of_bounds(cell):
+					continue
 
-			if not _passes_filter(grid.get_value(cell)):
-				continue
+				if not _passes_filter(grid.get_value(cell, layer)):
+					continue
 
-			if noise.get_noise_2d(cell.x, cell.y) > threshold:
-				grid.erase(cell)
+				if noise.get_noise_2d(cell.x, cell.y) > threshold:
+					grid.erase(cell, layer)
 
 
 func _is_out_of_bounds(cell: Vector2i) -> bool:
-	return (cell.x > max.x or cell.y > max.y or
-			cell.x < min.x or cell.y < min.y)
+	if not bounds_enabled:
+		return false
+	return (cell.x > bounds_max.x or cell.y > bounds_max.y or
+			cell.x < bounds_min.x or cell.y < bounds_min.y)
