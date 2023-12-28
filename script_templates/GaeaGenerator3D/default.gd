e8e8512944350ac4d127c94ea27ed0b271468358
benjatk <benjatorreskeselman@gmail.com>
2023-12-28 13:15:45 -0300

    Add script templates to project.
    Will make it easier to make new generators in the future.

---
+++ script_templates/GaeaGenerator3D/default.gd
@@ -0,0 +1,38 @@
+@tool
+extends GaeaGenerator3D
+
+
+# Change this to a custom resource class that extends GeneratorSettings3D.
+@export var settings: GeneratorSettings3D
+
+
+func generate(starting_grid: GaeaGrid = null) -> void:
+	if Engine.is_editor_hint() and not editor_preview:
+		push_warning("%s: Editor Preview is not enabled so nothing happened!" % name)
+		return
+
+	if not settings:
+		push_error("%s doesn't have a settings resource" % name)
+		return
+
+	var time_now: int = Time.get_ticks_msec()
+
+	generation_started.emit()
+
+	if starting_grid == null:
+		erase()
+	else:
+		grid = starting_grid
+
+	# Write your generation code here.
+
+	if is_instance_valid(next_pass):
+		next_pass.generate(grid)
+		return
+
+	var time_elapsed :int = Time.get_ticks_msec() - time_now
+	if OS.is_debug_build():
+		print("%s: Generating took %s seconds" % [name, float(time_elapsed) / 100 ])
+
+	grid_updated.emit()
+	generation_finished.emit()
