de09efd1893c7139eb6be520ebf8269d6e1bf119
Ian Cullum <cullumi@wwu.edu>
2024-05-30 08:33:40 -0700

    Added checkbox for turning on and off threading for ThreadedChunkLoaders

--- addons/gaea/others/threaded_chunk_loader_3d.gd
+++ addons/gaea/others/threaded_chunk_loader_3d.gd
@@ -3,6 +3,8 @@ class_name ThreadedChunkLoader3D
 extends ChunkLoader3D
 ## @experimental
 
+@export var threaded:bool = true
+
 var queued:Callable
 var task:int = -1
 
@@ -11,13 +13,21 @@ func _process(_delta):
 		if WorkerThreadPool.is_task_completed(task):
 			WorkerThreadPool.wait_for_task_completion(task)
 			task = -1
+			run_job(queued)
 	super(_delta)
 
 func _update_loading(actor_position: Vector3i) -> void:
-	var job:Callable = func ():
-		super._update_loading(actor_position)
-	
-	if task > -1:
-		queued = job
+	if not threaded:
+		super(actor_position)
 	else:
+		var job:Callable = func ():
+			super._update_loading(actor_position)
+		
+		if task > -1:
+			queued = job
+		else:
+			run_job(job)
+
+func run_job(job:Callable):
+	if job:
 		task = WorkerThreadPool.add_task(job, false, "Load/Unload Chunks")
