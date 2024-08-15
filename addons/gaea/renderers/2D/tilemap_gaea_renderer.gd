9c1e8e25195edb72e873ed0837290b42b937b970
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2024-08-15 17:45:30 -0300

    Squashed commit of the following:
    commit ad7937a86ce42e895b6fba99d74c564ffbe0e14d
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:48:41 2024 -0300
    
        v1.1.2
    
    commit 0e8c5bf8140b88c9bdcac38797a1883bfeebcf5e
    Merge: 85ca9c7 87c0278
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:48:16 2024 -0300
    
        Merge pull request #153 from BenjaTK/terrain-gap-fix
    
        Make terrain gap fix toggleable
    
    commit 87c027805b01031c4bf08e6a8531a1e11ca96cf8
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:46:52 2024 -0300
    
        Make variable more clear
    
    commit b96342f15808290b61951fd243a021968548b2ca
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:45:22 2024 -0300
    
        Make terrain gap fix toggleable
    
    commit 85ca9c7138363bb1d0ed7511d4d5fa1c0ac1fcfa
    Merge: 1e5ccbe 37bb89c
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:43:48 2024 -0300
    
        Merge pull request #152 from BenjaTK/air-layer
    
        Add air layer option in HeightmapGenerators
    
    commit 1e5ccbefc6b53af4946e31baa51c173b190e91e4
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:34:00 2024 -0300
    
        v1.1.1
    
    commit 6a71dc7d35eb70ae3b43ebd838adf3e7d4dbd34d
    Merge: a57fdd2 e653976
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:33:14 2024 -0300
    
        Merge pull request #151 from BenjaTK/bug
    
        Fix addon-breaking unnecesary code in modifier.gd
    
    commit e653976887a1c75ca1f0ae2ba65ad9fcad87e706
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:32:40 2024 -0300
    
        Fix addon-breaking unnecesary code in modifier.gd
    
    commit a57fdd2883e265e484a70d4488e5ab32571d5e70
    Merge: e27a44c a73044b
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:25:03 2024 -0300
    
        Merge pull request #150 from BenjaTK/terrain-gap-fix
    
        Fix gaps between terrains in TilemapRenderer
    
    commit a73044b16626249fcbeffcfdca0495135fea516b
    Author: BenjaTK <73806216+BenjaTK@users.noreply.github.com>
    Date:   Sun Aug 4 14:23:38 2024 -0300
    
        Fix gaps between terrains in TilemapRenderer

--- addons/gaea/renderers/2D/tilemap_gaea_renderer.gd
+++ addons/gaea/renderers/2D/tilemap_gaea_renderer.gd
@@ -14,6 +14,8 @@ extends GaeaRenderer2D
 @export var clear_tile_map_on_draw: bool = true
 ## Erases the cell when an empty tile is found in all layers. Recommended: [code]true[/code].
 @export var erase_empty_tiles: bool = true
+## Set this to [code]true[/code] if you have gaps between your terrains. Can cause problems.
+@export var terrain_gap_fix: bool = false
 
 
 func _ready() -> void:
@@ -64,7 +66,7 @@ func _draw_area(area: Rect2i) -> void:
 	for tile_info in terrains:
 		tile_map.set_cells_terrain_connect.call_deferred(
 			tile_info.tilemap_layer, terrains[tile_info],
-			tile_info.terrain_set, tile_info.terrain
+			tile_info.terrain_set, tile_info.terrain, !terrain_gap_fix
 		)
 
 	(func(): area_rendered.emit(area)).call_deferred()
