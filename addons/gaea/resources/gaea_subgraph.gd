06ef4e51368ea070bc7d5e1ecff38dc69f2c1a77
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-11-22 19:35:48 -0300

    Don't allow Parameters inside subgraphs

--- addons/gaea/resources/gaea_subgraph.gd
+++ addons/gaea/resources/gaea_subgraph.gd
@@ -18,18 +18,40 @@ func _init() -> void:
 
 
 func add_node(node: GaeaNodeResource, position: Vector2, id: int = get_next_available_id()) -> int:
-	id = super(node, position, id)
+	# The editor doesn't allow adding parameter nodes to subgraphs, but
+	# this is for pasted nodes.
+	if node is GaeaNodeParameter:
+		id = add_node(GaeaNodeSubGraphInput.new(), position, id)
+		set_node_enum.call_deferred(id, 0, node.get_type())
+		push_warning(
+			"Can't add GaeaNodeParameters to SubGraphs. Changed to an In node of the same type instead."
+		)
+		return id
 
+	id = super(node, position, id)
+	# Deferred so pasted nodes have the right name.
+	_on_node_added.call_deferred(node)
+
+	return id
+
+
+func add_node_with_data(node: GaeaNodeResource, data: Dictionary, id: int = get_next_available_id()) -> int:
+	if node is GaeaNodeParameter:
+		data.get_or_add(&"enums", []).append(node.get_type())
+		node = GaeaNodeSubGraphInput.new()
+
+	return super(node, data, id)
+
+
+func _on_node_added(node: GaeaNodeResource) -> void:
 	if node is GaeaNodeSubGraphInput:
 		inputs.set(
-			id, get_node_argument(id, &"name", node.get_argument_default_value(&"name"))
+			node.id, get_node_argument(node.id, &"name", node.get_argument_default_value(&"name"))
 		)
 	elif node is GaeaNodeSubGraphOutput:
 		outputs.set(
-			id, get_node_argument(id, &"name", node.get_argument_default_value(&"name"))
+			node.id, get_node_argument(node.id, &"name", node.get_argument_default_value(&"name"))
 		)
-
-	return id
 
 
 func remove_node(id: int) -> void:
