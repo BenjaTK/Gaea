568cab6df2853c254a275d202f8645a784f7c3f8
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2026-02-01 11:26:26 -0300

    Update to 4.6

--- addons/gaea_documentation_toolkit/plugin.gd
+++ addons/gaea_documentation_toolkit/plugin.gd
@@ -3,13 +3,20 @@ extends EditorPlugin
 
 const DocumentationToolkit = preload("uid://bxanwt5o3b0ng")
 
-var _panel: Control
+var _dock: EditorDock
 
 
 func _enter_tree() -> void:
-	_panel = DocumentationToolkit.instantiate()
-	add_control_to_bottom_panel(_panel, "Gaea Documentation Toolkit")
+	_dock = EditorDock.new()
+	_dock.available_layouts = EditorDock.DOCK_LAYOUT_FLOATING | EditorDock.DOCK_LAYOUT_HORIZONTAL
+	_dock.title = "Gaea Documentation Toolkit"
+	_dock.default_slot = EditorDock.DOCK_SLOT_BOTTOM
+
+	_dock.add_child(DocumentationToolkit.instantiate())
+	add_dock(_dock)
 
 
 func _exit_tree() -> void:
-	remove_control_from_bottom_panel(_panel)
+	remove_dock(_dock)
+	_dock.queue_free()
+	_dock = null
